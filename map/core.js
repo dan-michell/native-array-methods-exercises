43e24f5df89056b9bac760b3d91a7f68c134ffb7
Dan Michell <dan.michell1000@gmail.com>
2022-03-24 12:29:33 +0000

    Completed map exercises

--- map/core.js
+++ map/core.js
@@ -1,22 +1,34 @@
-function multiplyBy10 (array) {
- // your code here
-};
+function multiplyBy10(array) {
+    // your code here
+    return array.map((num) => num * 10);
+}
 
-function shiftRight (array) {
- // your code here
-};
+function shiftRight(array) {
+    // your code here
+    return array.map((ele, index, arr) => {
+        return arr[(index + 2) % arr.length];
+    });
+}
 
-function onlyVowels (array) {
- // your code here
-};
+function onlyVowels(array) {
+    // your code here
+    return array.map((word) => {
+        return word.replace(/[b-df-hj-np-tv-z]+/g, "");
+    });
+}
 
-function doubleMatrix (array) {
- // your code here
-};
+function doubleMatrix(array) {
+    // your code here
+    return array.map((row) => {
+        return row.map((ele) => {
+            return ele * 2;
+        });
+    });
+}
 
 module.exports = {
-  multiplyBy10: multiplyBy10,
-  shiftRight: shiftRight,
-  onlyVowels: onlyVowels,
-  doubleMatrix: doubleMatrix
+    multiplyBy10: multiplyBy10,
+    shiftRight: shiftRight,
+    onlyVowels: onlyVowels,
+    doubleMatrix: doubleMatrix,
 };
